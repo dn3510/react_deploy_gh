8eb59b365943655fc6f81b345875be9bfcfa7ad2
dn3510 <dn3510@gmail.com>
2022-12-08 19:06:37 +0800

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./Header";
+import AddItem from "./AddItem";
+import SearchItem from "./SearchItem";
+import Content from "./Content";
+import Footer from "./Footer";
+import { useState, useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [items, setItems] = useState(
+		JSON.parse(localStorage.getItem("shoppinglist")) || []
+	);
+	const [newItem, setNewItem] = useState("");
+	const [search, setSearch] = useState("");
+
+	useEffect(() => {
+		localStorage.setItem("shoppinglist", JSON.stringify(items));
+	}, [items]);
+
+	const addItem = (item) => {
+		const id = items.length ? items[items.length - 1].id + 1 : 1;
+		const myNewItem = { id, checked: false, item };
+		const listItems = [...items, myNewItem];
+		setItems(listItems);
+	};
+
+	const handleCheck = (id) => {
+		const listItems = items.map((item) =>
+			item.id === id ? { ...item, checked: !item.checked } : item
+		);
+		setItems(listItems);
+	};
+
+	const handleDelete = (id) => {
+		const listItems = items.filter((item) => item.id !== id);
+		setItems(listItems);
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (!newItem) return;
+		addItem(newItem);
+		setNewItem("");
+	};
+
+	return (
+		<div className="App">
+			<Header title="Grocery List" />
+			<AddItem
+				newItem={newItem}
+				setNewItem={setNewItem}
+				handleSubmit={handleSubmit}
+			/>
+			<SearchItem search={search} setSearch={setSearch} />
+			<Content
+				items={items.filter((item) =>
+					item.item.toLowerCase().includes(search.toLocaleLowerCase())
+				)}
+				handleCheck={handleCheck}
+				handleDelete={handleDelete}
+			/>
+			<Footer length={items.length} />
+		</div>
+	);
 }
 
 export default App;
